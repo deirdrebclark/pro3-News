a1652ecd7087daf383eb2d7aab40428832b5eaf7
Siraj2493 <pirzadasir@gmail.com>
2020-04-26 17:18:33 -0400

    updated Flask

--- sentiment.py
+++ sentiment.py
@@ -14,24 +14,6 @@ mongo = PyMongo(app)
 # Or set inline
 # mongo = PyMongo(app, uri="mongodb://localhost:27017/pro3news")
 
-# def get_chart(data):
-
-#     layout = plotly.graph_objs.Layout(title='Coronavirus Sentiment in Newspapers')
-#     scatter_data = [
-#         plotly.graph_objs.Scatter(
-#             # x=[d['date'] for d in data],
-#             x=[d['sentiment'] for d in data],
-#             y=[d['sentiment'] for d in data]
-#         )
-#     ]
-#     fig = plotly.graph_objs.Figure(data=scatter_data, layout=layout)
-#     return plotly.offline.plot(fig, include_plotlyjs=True, output_type='div')
-
-# @app.route('/sentiment')
-# def chart():
-#     sentiment = mongo.db.sentiment.find()
-#     print(sentiment)
-#     return render_template('sentiment.html',sentiment = sentiment)
 
 @app.route("/sentiment")
 def index():
