87ac419ef8f2ef8f8fe1dd164016f36087170903
deirdrebclark <deirdrebclark@gmail.com>
2020-04-27 00:54:52 -0400

    Add index.html

--- shares.py
+++ shares.py
@@ -1,62 +1,58 @@
-from flask import Flask, render_template, jsonify
-import json
-import response
-
+from flask import Flask, render_template, jsonify, request, redirect
+import flask
+import json, response
+from json import dumps
+from bson import json_util
 
 # Import our pymongo library, which lets us connect our Flask app to our Mongo database.
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 
-# Create an instance of our Flask app.
 app = Flask(__name__)
+CORS(app)
 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/pro3news"
 mongo = PyMongo(app)
 
-# Create connection variable
-#conn = 'mongodb://localhost:27017'
-
-# Pass connection to the pymongo instance.
-#client = PyMongo.MongoClient(conn)
-
-# Connect to a database. 
-#db = client.pro3news
-#collection = db.shares
-
-
 # Set route
 
 data = {}
-@app.route('/shares')
+@app.route('/shares', methods=['GET','POST'])
 def news_shares():
+    print('inside news shares')
+    if request.method == "POST":
+        if request.form['weekday'] > "":
+            weekday = request.form['weekday']
+            return weekday
+            print("Input")
+            print(weekday)
     # Pull the newspaper facebook shares
-    newspaper = mongo.db.shares.find()
-    return render_template('shares.html',) 
+    if request.method == 'GET':
+        
+        newspapers = mongo.db.shares.find()
+        #return redirect('/shares_data')
+        return render_template('shares.html', popNews = newspapers) 
+        #newspapers = list(mongo.db.shares.find())
+        #newspaper = json.dumps(newspapers)
+        #shares = [json.dumps(newspaper, default=json_util.default) for newspaper in Cursor]
+        
+        
+#@app.route('/shares_data')
+#def shares_data():
 
-    print(jsonify(newspaper))
-    
-    #result = newspaper.find_one({'newspaper':newspaper.newspaper,'date':newspaper.date,'url':newspaper.url,'title':newspaper.title,'shares':newspaper.shares})
+        
+        #return json.dumps(newspapers, default=json_util.default)
+        #news = json.dumps(newspapers, default=json_util.default)
+        #return shares
+  
+        #return jsonify(shares=news)
 
-    #return jsonify({'results': result['values']})
-    #
-    # this one works
-    #for news in newspaper:
-       # js = list({'newspaper':news.newspaper,'date':news.date,'url':news.url,'title':news.title,'shares':news.shares})
 
-    # Return the template with the shares passed in
-    #return render_template('index.html', shares = newspaper)
+@app.route('/action_page.php')
+def form_post():
+    return redirect('/shares') 
 
-    #return jsonify({'results':result('newspaper','date','url','title','shares')})
-    #return jsonify({'paper':'newspaper'})
-    #return response(json.dumps(js), mimetype='application/json')
-    #
-    # 
-    # this one works
-    #return jsonify(js['values'])
 
-    #
-    # 
-    # this one works, too
-    #return jsonify({'values':js})
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/shares.js
+++ static/js/shares.js
@@ -1,1 +1,139 @@
-console.log(data);
+const data_file=('../static/js/shares_objects.json');
+
+d3.json(data_file).then (function (data) {
+    createChart(data); 
+});
+
+function createChart(data) {
+
+    const start = dateFns.startOfWeek(new Date('2020-03-01')).toISOString().split('T')[0];
+    
+    const end = dateFns.endOfWeek(new Date('2020-03-01')).toISOString().split('T')[0];
+
+    console.log(start);
+    console.log(end);
+
+    var news = Object.keys(data);
+    var len = news.length
+    const reqData = {};
+    for (let i = 0; i < len; i++) {
+        if (data[i].date >= (start) && data[i].date <= (end)) {
+            // reqData['newspaper'].push(Object.values{d});
+            // reqData['date'].push(date[i].date);
+            // reqData['url'].push(date[i].url);
+            // reqData['title'].push(date[i].title);
+            // reqData['shares'].push(date[i].shares);
+        }
+
+    }; 
+
+
+        let reqBbData = [];
+        for (let j = 1; j < len; j++) {
+            console.log(Object.keys(reqData[j].newspaper))
+            console.log(reqData[j].newspaper)
+            console.log(Object.keys(reqData[j].newspaper))
+            if (reqData[j].newspaper == 'Breitbart') {
+                reqBbData.push(reqData[j]);
+            }
+        };
+       
+    console.log(reqData[0].newspaper);
+
+
+
+
+
+    let reqBiData = (reqData => reqData.newspaper == 'Business_Insider');
+    let reqFnData = (reqData => reqData.newspaper == 'Fox News');
+    let reqFtData = (reqData => reqData.newspaper == 'Financial Times');
+    let reqMwData = (reqData => reqData.newspaper == 'MarketWatch');
+    let reqNyData = (reqData => reqData.newspaper == 'NYTimes');
+    let reqRtData = (reqData => reqData.newspaper == 'Reuters');
+    let reqTgData = (reqData => reqData.newspaper == 'The Gaurdian');
+    let reqWjData = (reqData => reqData.newspaper == 'WSJ');
+   
+    Chart.defaults.global.defaultFontFamily = 'Lato';
+    Chart.defaults.global.defaultFontSize = 12;
+    Chart.defaults.global.defaultFontColor = '#777';
+
+    const chart = document.getElementById('newsChart').getContext('2d');
+
+    let newsChart = new Chart(chart, {
+        type: 'line',
+        data: {
+            labels: [reqData.newspaper],
+            datasets: [
+                {
+                 label:'Breitbart',
+                 data: [reqBbData.shares]
+                },
+                {
+                label:'Business Insider',
+                data: [reqBiData.shares]   
+                },
+                {
+                label:'Fox News',
+                data: [reqFnData.shares]   
+                },
+                {
+                label:'Financial Times',
+                data: [reqFtData.shares]   
+                },
+                {
+                label:'MarketWatch',
+                data: [reqMwData.shares]   
+                },
+                {
+                label:'New York Times',
+                data: [reqNyData.shares]   
+                },
+                {
+                label:'Reuters',
+                data: [reqRtData.shares]   
+                },
+                {
+                label:'The Guardian',
+                data: [reqTgData.shares]   
+                },
+                {
+                label:'Wall Street Journal',
+                data: [reqWjData.shares]   
+                },
+            ]
+            
+        },
+        options: {
+            title: {
+                display:true,
+                text:'Newspaper Facebook Shares',
+                fontSize:10
+            },
+            legend: {
+                position:'right',
+                labels: {
+                    fontColor:'#000'
+                }
+            },
+            layout: {
+                padding: {
+                    left:50,
+                    right:0,
+                    bottom:0,
+                    top:0
+
+                }
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+
+                    }
+                }]
+            },
+        }
+    });
+
+};
+
