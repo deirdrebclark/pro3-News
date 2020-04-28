13ff80c07dc714c7473cfce7029d748e02c5f8e6
deirdrebclark <deirdrebclark@gmail.com>
2020-04-28 16:39:13 -0400

    Updated shares chart

--- static/js/shares.js
+++ static/js/shares.js
@@ -1,105 +1,227 @@
-const data_file=('../static/js/shares_objects.json');
+const data_file=('../static/js/shares5.json');
 
 d3.json(data_file).then (function (data) {
-    createChart(data); 
+    var stories = [];
+
+    Object.keys(data).forEach(function(item) {
+      stories.push(data[item]);
+    });
+
+    createChart(stories); 
 });
 
 function createChart(data) {
 
     const start = dateFns.startOfWeek(new Date('2020-03-01')).toISOString().split('T')[0];
-    
     const end = dateFns.endOfWeek(new Date('2020-03-01')).toISOString().split('T')[0];
 
-    console.log(start);
-    console.log(end);
+    var reqData = [];
+    for (i=0; i < data.length; i++) {
+        if (data[i].date >= start && data[i].date <= end) {
+            reqData.push(data[i]);
+         };
+    };
 
-    var news = Object.keys(data);
-    var len = news.length
-    const reqData = {};
-    for (let i = 0; i < len; i++) {
-        if (data[i].date >= (start) && data[i].date <= (end)) {
-            // reqData['newspaper'].push(Object.values{d});
-            // reqData['date'].push(date[i].date);
-            // reqData['url'].push(date[i].url);
-            // reqData['title'].push(date[i].title);
-            // reqData['shares'].push(date[i].shares);
-        }
-
-    }; 
-
-
-        let reqBbData = [];
-        for (let j = 1; j < len; j++) {
-            console.log(Object.keys(reqData[j].newspaper))
-            console.log(reqData[j].newspaper)
-            console.log(Object.keys(reqData[j].newspaper))
-            if (reqData[j].newspaper == 'Breitbart') {
-                reqBbData.push(reqData[j]);
-            }
+    var reqBbData = [];
+    for (i=0; i < reqData.length; i++) {
+        if (reqData[i].newspaper == 'Breitbart') {
+            reqBbData.push(reqData[i])
         };
-       
-    console.log(reqData[0].newspaper);
+    };
 
-
-
-
-
-    let reqBiData = (reqData => reqData.newspaper == 'Business_Insider');
-    let reqFnData = (reqData => reqData.newspaper == 'Fox News');
-    let reqFtData = (reqData => reqData.newspaper == 'Financial Times');
-    let reqMwData = (reqData => reqData.newspaper == 'MarketWatch');
-    let reqNyData = (reqData => reqData.newspaper == 'NYTimes');
-    let reqRtData = (reqData => reqData.newspaper == 'Reuters');
-    let reqTgData = (reqData => reqData.newspaper == 'The Gaurdian');
-    let reqWjData = (reqData => reqData.newspaper == 'WSJ');
+    var reqBiData = [];
+    for (i=0; i < reqData.length; i++) {
+        if (reqData[i].newspaper == 'Business_Insider') {
+            reqBiData.push(reqData[i])
+        };
+    };
+    var reqFnData = [];
+    for (i=0; i < reqData.length; i++) {
+        if (reqData[i].newspaper == 'Fox News') {
+            reqFnData.push(reqData[i])
+        };
+    };
+    var reqFtData = [];
+    for (i=0; i < reqData.length; i++) {
+        if (reqData[i].newspaper == 'Financial Times') {
+            reqBbData.push(reqData[i])
+        };
+    };
+    var reqMwData = [];
+    for (i=0; i < reqData.length; i++) {
+        if (reqData[i].newspaper == 'MarketWatch') {
+            reqMwData.push(reqData[i])
+        };
+    };
+    var reqNyData = [];
+    for (i=0; i < reqData.length; i++) {
+        if (reqData[i].newspaper == 'NYTimes') {
+            reqNyData.push(reqData[i])
+        };
+    };
+    var reqRtData = [];
+    for (i=0; i < reqData.length; i++) {
+        if (reqData[i].newspaper == 'Reuters') {
+            reqRtData.push(reqData[i])
+        };
+    };
+    var reqTgData = [];
+    for (i=0; i < reqData.length; i++) {
+        if (reqData[i].newspaper == 'The Gaurdian') {
+            reqTgData.push(reqData[i])
+        }
+    };
+    var reqWjData = [];
+    for (i=0; i < reqData.length; i++) {
+        if (reqData[i].newspaper == 'WSJ') {
+            reqWjData.push(reqData[i])
+        }
+    };
    
-    Chart.defaults.global.defaultFontFamily = 'Lato';
-    Chart.defaults.global.defaultFontSize = 12;
-    Chart.defaults.global.defaultFontColor = '#777';
+    var BbShares = 0
+    for (i=0; i<reqBbData.length; i++) {
+        BbShares += reqBbData[i].shares 
+    };
 
-    const chart = document.getElementById('newsChart').getContext('2d');
+
+    var chartDate = addDays(start, 2)
+    function addDays(start, days) {
+        var result = new Date(start);
+        result.setDate(result.getDate() + days);
+        return result;
+      }
+    chartDateM = new Date(chartDate).toISOString().split('T')[0];
+    var chartDate = addDays(start, 3)
+    function addDays(start, days) {
+        var result = new Date(start);
+        result.setDate(result.getDate() + days);
+        return result;
+      }
+    chartDateT = new Date(chartDate).toISOString().split('T')[0];
+    
+    var chartDate = addDays(start, 4)
+    function addDays(start, days) {
+        var result = new Date(start);
+        result.setDate(result.getDate() + days);
+        return result;
+      }
+    chartDateW = new Date(chartDate).toISOString().split('T')[0];
+    var chartDate = addDays(start, 5)
+    function addDays(start, days) {
+        var result = new Date(start);
+        result.setDate(result.getDate() + days);
+        return result;
+      }
+    chartDateTh = new Date(chartDate).toISOString().split('T')[0];
+    var chartDate = addDays(start, 6)
+    function addDays(start, days) {
+        var result = new Date(start);
+        result.setDate(result.getDate() + days);
+        return result;
+      }
+    chartDateF = new Date(chartDate).toISOString().split('T')[0];
+    var chartDate = addDays(start, 2)
+    function addDays(start, days) {
+        var result = new Date(start);
+        result.setDate(result.getDate() + days);
+        return result;
+      }
+    
+      
+      
+    Chart.defaults.global.defaultFontFamily = '#000';
+    Chart.defaults.global.defaultFontSize = 14;
+    Chart.defaults.global.defaultFontColor = '#000';
+    Chart.defaults.global.defaultBackground = 'White';
+    
+
+    var chart = document.getElementById('newsChart').getContext('2d');
 
     let newsChart = new Chart(chart, {
         type: 'line',
         data: {
-            labels: [reqData.newspaper],
+            labels: [start
+                    ,chartDateM
+                    ,chartDateT
+                    ,chartDateW
+                    ,chartDateTh
+                    ,chartDateF
+                    ,end
+                ],
             datasets: [
                 {
                  label:'Breitbart',
-                 data: [reqBbData.shares]
+                 data: [reqBbData[0].shares
+                       ,reqBbData[1].shares
+                       ,reqBbData[2].shares
+                       ,reqBbData[3].shares
+                       ,reqBbData[4].shares
+                    ]
                 },
-                {
+               /* {
                 label:'Business Insider',
-                data: [reqBiData.shares]   
-                },
+                data: [reqBiData[0].shares
+                ,reqBiData[1].shares
+                ,reqBiData[2].shares
+                ,reqBiData[3].shares
+                ,reqBiData[4].shares]   
+                },*/
                 {
                 label:'Fox News',
-                data: [reqFnData.shares]   
+                data: [reqFnData[0].shares
+                ,reqFnData[1].shares
+                ,reqFnData[2].shares
+                ,reqFnData[3].shares
+                ,reqFnData[4].shares]   
                 },
-                {
+                /*{
                 label:'Financial Times',
-                data: [reqFtData.shares]   
-                },
-                {
+                data: [reqFtData[0].shares
+                ,reqFtData[1].shares
+                ,reqFtData[2].shares
+                ,reqFtData[3].shares
+                ,reqFtData[4].shares]   
+                },*/
+                /*{
                 label:'MarketWatch',
-                data: [reqMwData.shares]   
-                },
-                {
+                data: [reqMwData[0].shares
+                ,reqMwData[1].shares
+                ,reqMwData[2].shares
+                ,reqMwData[3].shares
+                ,reqMwData[4].shares]   
+                },*/
+                /*{
                 label:'New York Times',
-                data: [reqNyData.shares]   
-                },
+                data: [reqNyData[0].shares
+                ,reqNyData[1].shares
+                ,reqNyData[2].shares
+                ,reqNyData[3].shares
+                ,reqNyData[4].shares]   
+                },*/
                 {
                 label:'Reuters',
-                data: [reqRtData.shares]   
+                data: [reqRtData[0].shares
+                ,reqRtData[1].shares
+                ,reqRtData[2].shares
+                ,reqRtData[3].shares
+                ,reqRtData[4].shares]   
                 },
-                {
+                /*{
                 label:'The Guardian',
-                data: [reqTgData.shares]   
-                },
-                {
+                data: [reqTgData[0].shares
+                ,reqTgData[1].shares
+                ,reqTgData[2].shares
+                ,reqTgData[3].shares
+                ,reqTgData[4].shares]   
+                },*/
+                /*{
                 label:'Wall Street Journal',
-                data: [reqWjData.shares]   
-                },
+                data: [reqWjData[0].shares
+                ,reqWjData[1].shares
+                ,reqWjData[2].shares
+                ,reqWjData[3].shares
+                ,reqWjData[4].shares]   
+                },*/
             ]
             
         },
